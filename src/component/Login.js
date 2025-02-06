e8d5234c98e69b6156ad8b07b74e7e52f65ec152
Md07md <dhamechamahendra7777@gmail.com>
2025-02-06 18:00:36 +0530

    second commit

--- src/component/Login.js
+++ src/component/Login.js
@@ -6,54 +6,91 @@ import axios from 'axios';
 
 const Login = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [credencial,setCredencial] = useState({email:"", password:""})
+  const [errorMessage, setErrorMessage] = useState({validEmail:"",validPass:""});
+  const [credencialError, setCredencialError] = useState({incorrectEmail:"",incorrectPass:""});
   let navigate = useNavigate();
   localStorage.removeItem('user');
   localStorage.removeItem("isAuthenticated");
 
+  const onChange = (e) => {
+    setCredencial({...credencial, [e.target.name]: e.target.value});
+  };
+
+  const validateInput = () => {
+    const newErrors = {};
+
+    if (!credencial.email) {
+      newErrors.validEmail = '*Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(credencial.email)) {
+      newErrors.validEmail = '*Email is invalid';
+    }
+
+    if (!credencial.password) {
+      newErrors.validPass = '*Password is required';
+    }
+    setErrorMessage(newErrors);
+    // Return true if no errors
+    return Object.keys(newErrors).length === 0;
+  };
+
   const handleSubmit = async (e)=>{
-
     e.preventDefault();
-    const API_URL = "https://67a31cd8409de5ed52576fba.mockapi.io/user";
+    validateInput();
+    if(validateInput()){
 
-    try {
+    const API_URL = "https://67a31cd8409de5ed52576fba.mockapi.io/user";
+    let error={};
+    try {        
         // Make an API call to the mock API endpoint
-        const response = await axios.get(API_URL, { params: { email: email }});
+        const response = await axios.get(API_URL, { params: { email: credencial.email }});
 
         // Check if user exists and credentials are correct
           if (response.data.length > 0) {
-            if(password===response.data[0].password){
+            if(credencial.password===response.data[0].password){
               localStorage.setItem("user", JSON.stringify(response.data));
               localStorage.setItem("isAuthenticated", "true");
               navigate("/");
-            }else{
-              alert('Invalid Password');
+            }else{             
+              error.incorrectPass="* Please Enter Correct Password";
+              setCredencialError(error);
             };
           } 
         } catch (error) {
-          alert('Invalid Credencials');
+          error.incorrectPass="* Please Enter Correct Credencial";
+          setCredencialError(error);
         }
+      }
     };
 
   return (
     <div className="login-body">
       <div className="login-main">
-      <h2>Login</h2>
-        <div className="login-form">       
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" name="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" aria-describedby="emailHelp" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" name="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" autoComplete="off" placeholder="Password" />
-                </div>
-                <button type="submit" className="btn btn-primary text-center" >Submit</button>
-                <p className="signup-link">Don't have an account? <Link to="/signup">Sign up</Link></p>
-            </form>
-          </div>
+        <h2>Login</h2>
+
+        {credencialError.incorrectEmail && <div style={{ color: 'red',textAlign:'center', marginTop: '5px'}}>{credencialError.incorrectEmail}</div>}
+        {credencialError.incorrectPass && <div style={{ color: 'red',textAlign:'center', marginTop: '5px'}}>{credencialError.incorrectPass}</div>}
+          
+          <div className="login-form">       
+              <form onSubmit={handleSubmit}>
+
+                  <div className="form-group">
+                      <label htmlFor="email">Email address</label>
+                      <input type="email" className="form-control" name="email" value={credencial.email} onChange={onChange} id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                      {errorMessage.validEmail && <div style={{ color: 'red', marginTop: '5px', fontSize:'small'}}>{errorMessage.validEmail}</div>}
+                  </div>
+
+                  <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <input type="password" className="form-control" name="password" value={credencial.password} onChange={onChange} id="password" autoComplete="off" placeholder="Password" />
+                      {errorMessage.validPass && <div style={{ color: 'red', marginTop: '5px', fontSize:'small'}}>{errorMessage.validPass}</div>}
+                  </div>
+                  
+                  <button type="submit" className="btn btn-primary text-center" >Submit</button>
+                  <p className="signup-link">Don't have an account? <Link to="/signup">Sign up</Link></p>
+                  
+              </form>
+            </div>
       </div>
     </div>
   )
--- src/component/Signup.js
+++ src/component/Signup.js
@@ -5,16 +5,45 @@ import axios from 'axios';
 
 const Signup = () => {
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [credencial,setCredencial] = useState({name:"", email:"", password:""})
+  const [errorMessage, setErrorMessage] = useState({validName:"",validEmail:"",validPass:"",validUser:""});
   let navigate = useNavigate();
+
+  const onChange = (e) => {
+    setCredencial({...credencial, [e.target.name]: e.target.value});
+  };
+
+  const validateInput = () => {
+    const newErrors = {};
+
+    if (!credencial.name) {
+      newErrors.validName = '*Name is required';
+    }else if (credencial.name.length < 3) {
+      newErrors.validName = '*Name must be at least 3 characters';
+    }
+
+    if (!credencial.email) {
+      newErrors.validEmail = '*Email is required';
+    } else if (!/\S+@\S+\.\S+/.test(credencial.email)) {
+      newErrors.validEmail = '*Email is invalid';
+    }
+
+    if (!credencial.password) {
+      newErrors.validPass = '*Password is required';
+    } else if (credencial.password.length < 6) {
+      newErrors.validPass = '*Password must be at least 6 characters';
+    }
+    setErrorMessage(newErrors);
+    // Return true if no errors
+    return Object.keys(newErrors).length === 0;
+  };
+
   const API_URL = "https://67a31cd8409de5ed52576fba.mockapi.io/user";
 
   const checkEmailExists = async (email) => {
     try {
       const response = await axios.get(API_URL, {
-            params: { email }, // query parameter to search by email
+            params: { email : credencial.email }, // query parameter to search by email
           });     
       if (response) {
         return response.data.length > 0;
@@ -28,44 +57,48 @@ const Signup = () => {
 
   const handleSubmit = async(e) =>{
     e.preventDefault();
-    try {
-      const emailExists = await checkEmailExists(email);
-      if (emailExists) {
-        alert("user already exists")
-      }else{     
-        const response = await axios.post(API_URL, { name, email, password });
-        if(response){  
-          navigate("/login");           
-        }
-      }} catch (error) {
-        throw error;
-      }
-    
+    let errorUser={}
+    validateInput();
+    if(validateInput()){
+      try {
+        const emailExists = await checkEmailExists(credencial.email);
+        if (emailExists) {
+          errorUser.validUser="* User Already Exists";
+          setErrorMessage(errorUser);
+        }else{     
+          const response = await axios.post(API_URL, 
+            { name:credencial.name, email: credencial.email, password: credencial.password }
+          );
+          if(response){  
+            navigate("/login");           
+          }
+        }} catch (error) {
+          throw error;
+        }  
+    }
   }
-
-  // const onChange =(e)=>{
-  //   setEmail({...email, [e.target.name]: e.target.value});
-  //   setName({...name, [e.target.name]: e.target.value});
-  //   setPassword({...password, [e.target.name]: e.target.value});
-  // }
 
   return (
     <div className="signup-body">
       <div className="signup-main">
       <h2>Signup</h2>
+      {errorMessage.validUser && <div style={{ color: 'red',textAlign:'center', marginTop: '5px' }}>{errorMessage.validUser}</div>}
         <div className="signup-form">       
           <form onSubmit={handleSubmit}>
               <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder="name" />
+                <input type="text" className="form-control" id="name" name='name' value={credencial.name} onChange={onChange} placeholder="name" />
+                {errorMessage.validName && <div style={{ color: 'red', marginTop: '5px',fontSize:'small' }}>{errorMessage.validName}</div>}
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email address</label>
-                <input type="email" className="form-control"  id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} aria-describedby="emailHelp" placeholder="Enter email" />
+                <input type="email" className="form-control"  id="email" name='email' value={credencial.email} onChange={onChange} aria-describedby="emailHelp" placeholder="Enter email" />
+                {errorMessage.validEmail && <div style={{ color: 'red', marginTop: '5px', fontSize:'small'}}>{errorMessage.validEmail}</div>}
               </div>  
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" name='password' autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                <input type="password" className="form-control" id="password" name='password' autoComplete="off" value={credencial.password} onChange={onChange} placeholder="Password" />
+                {errorMessage.validPass && <div style={{ color: 'red', marginTop: '5px' ,fontSize:'small'}}>{errorMessage.validPass}</div>}
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
               <p className="login-link">Already have an account? <Link to="/login">Login</Link></p>
